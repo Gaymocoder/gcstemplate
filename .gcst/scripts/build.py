3feba51b9803fac8aac26a3a1cdbf2d3976dc77f
Gaymocoder <gaymocoder@gmail.com>
2026-08-28 02:01:15 +0300

    B&W | Added default preset remembering
    — Created ".gcst/.default"-file holding the last used preset:
        — build.py saves the given preset into it;
        — build.py reads it when "--preset" is omitted;
        — build.py aborts when neither is given;
    
    — Added ".gcst/.default" into .gitignore.

--- .gcst/scripts/build.py
+++ .gcst/scripts/build.py
@@ -11,8 +11,13 @@ root_path = Path(subprocess.check_output(['git', '-C', HERE, 'rev-parse', '--sho
 bin_dir = root_path/"bin"
 gcst_dir = root_path/".gcst"
 build_dir = root_path/"build"
-
 configure_py = gcst_dir/"scripts"/"configure.py"
+
+defaultPreset = ".default"
+defaultPresetPath = gcst_dir/".default"
+
+# TODO: default preset
+# TODO: auto-detect preset
 
 def getArgs():
     argvParser = argparse.ArgumentParser(prog = 'gcst-builder')
@@ -21,8 +26,20 @@ def getArgs():
     argvParser.add_argument('-il', '--ignore-local', action = 'store_true')
     args = argvParser.parse_args()
     if args.preset == None:
-        args.preset = "default"
+        args.preset = getDefaultPreset()
     return args
+
+
+def getDefaultPreset():
+    if not defaultPresetPath.exists():
+        return None
+    
+    with open(defaultPresetPath, 'r', encoding = 'utf-8') as f:
+        return f.read()
+
+def setDefaultPreset(preset):
+    with open(defaultPresetPath, 'w', encoding = 'utf-8') as f:
+        return f.write(preset)
 
     
 def clear_build_directory():
@@ -54,7 +71,7 @@ def conan_install(profile):
     
 def cmake(preset):
     command = ["cmake"]
-    if preset == "default":
+    if preset == ".default":
         command.extend(["-B", build_dir, "-S", root_path])
     else:
         command.extend(["--preset", preset])
@@ -71,11 +88,9 @@ def main():
     args = getArgs()
     clear = args.clear
     preset = args.preset
-
-    conan_dir = root_path/"conan"/"profiles"
-    conan_profile = preset
-    if preset != "default":
-        conan_profile = conan_dir/preset
+    if not preset:
+        print("Build-preset was not specified, but no default preset is set. Aborting")
+        return 1
 
     if clear:
         clear_build_directory()
@@ -85,23 +100,32 @@ def main():
     if result.returncode != 0:
         print(f"Configure failed with code {result.returncode}")
         print("Executed command:\n", *result.args)
-        return 1
-    
+        return 2
+
+    conan_dir = root_path/"conan"/"profiles"
+    conan_profile = conan_dir/preset
+    if not conan_profile.exists():
+        print(f"No specified build-preset ({preset}) was found. Aborting")
+        return 3
+
+    setDefaultPreset(preset)
     result = conan_install(conan_profile)
     if result.returncode != 0:
         print(f"Conan install failed with code {result.returncode}")
         print("Executed command:\n", *result.args)
-        return 2
+        return 4
     
     result = cmake(preset)
     if result.returncode != 0:
         print("Executed command:\n", *result.args)
-        return 3
+        return 5
     
     result = cmake_build()
     if result.returncode != 0:
         print("Executed command:\n", *result.args)
-        return 4
+        return 6
+
+    return 0
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
