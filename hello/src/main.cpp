b825c54b5cf85a1131b2fd827be7de0d002c9944
Gaymocoder <gaymocoder@gmail.com>
2026-09-15 21:08:14 +0300

    B&NW | Separated showcase from "hello"-exe
    — Renamed "Hello"-dir into "hello";
    — Moved showcase from main() into gcst::showcase::std_print();
    — Created "gcst_hello_showcase"-object-library for showcase functions;

---
+++ hello/src/main.cpp
@@ -0,0 +1,7 @@
+#include "gcst/showcase.h"
+
+int main(int argc, char** argv)
+{
+    gcst::showcase::std_print();
+    return 0;
+}
---
+++ include/gcst/showcase.h
@@ -0,0 +1,9 @@
+#ifndef __GCST_SHOWCASE__
+#define __GCST_SHOWCASE__
+
+namespace gcst::showcase
+{
+    void std_print();
+}
+
+#endif
