d95e98ad341a3a82ffc51068cd2a01596863a67d
Gaymocoder <gaymocoder@gmail.com>
2026-09-16 04:05:55 +0300

    Fixes: include in namespace and nullptr

--- utils/src/exstd.cpp
+++ utils/src/exstd.cpp
@@ -1,5 +1,9 @@
 #include "gcst/utils/exstd.h"
 
+#ifdef _WIN32
+    #include <windows.h>
+#endif
+
 #include <string>
 
 namespace gcst::utils::exstd
@@ -7,13 +11,12 @@ namespace gcst::utils::exstd
 
 #ifdef _WIN32
 
-#include <windows.h>
 fs::path exe_path()
 {    
     std::wstring wstrpath(MAX_PATH, L'\0');
     while (true)
     {
-        DWORD total_wchars = GetModuleFileNameW(NULL, wstrpath.data(), MAX_PATH);
+        DWORD total_wchars = GetModuleFileNameW(nullptr, wstrpath.data(), MAX_PATH);
         if (total_wchars == 0)
             return {};
         if (total_wchars < wstrpath.size())
