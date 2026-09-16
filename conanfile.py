864065d2351827fa637daa5ffbb045b9b1462914
Gaymocoder <gaymocoder@gmail.com>
2026-09-16 22:53:31 +0300

    B&W | Added exporting local Conan recipes
    — Recipes from "recipes/"-dir are exported in build.py with versions from conanfile.py requires;
    — Moved requirements into "requires"-attribute of conanfile.py for "conan inspect";
    
    — Added "recipes/**" into Conan cache key in CI.

--- conanfile.py
+++ conanfile.py
@@ -4,9 +4,10 @@ from conan.tools.cmake import CMakeToolchain, CMakeDeps
 class gcstConan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     default_options = {"boost/*:header_only": True}
-    
-    def requirements(self):
-        self.requires("boost/1.87.0")
+
+    requires = (
+        "boost/1.87.0"
+    )
 
     def generate(self):
         CMakeToolchain(self).generate()
